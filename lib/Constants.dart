5973b55b26128419766cb94eb39accf60fc9db0e
CodeRonin404 <tukusalifyasichali@gmail.com>
2026-06-28 00:44:41 +0200

    ADDED more constants,

--- lib/Constants.dart
+++ lib/Constants.dart
@@ -10,6 +10,36 @@ class constants
   static const Color lightblue = Color.fromRGBO(200, 237, 250, 0.8);
   static const Color lightpurple = Color.fromRGBO(231, 219, 255, 0.76);
   static const Color blue = Color.fromRGBO(63, 81, 181, 1);
+}
 
-
+class AppColors {
+  AppColors._();
+  static const Color primary = Color(0xFF924C00);
+  static const Color onPrimary = Color(0xFFFFFFFF);
+  static const Color primaryContainer = Color(0xFFFF8800);
+  static const Color onPrimaryContainer = Color(0xFF613000);
+  static const Color secondary = Color(0xFF835500);
+  static const Color onSecondary = Color(0xFFFFFFFF);
+  static const Color secondaryContainer = Color(0xFFFEB64C);
+  static const Color background = Color(0xFFFFF8F5);
+  static const Color onBackground = Color(0xFF241912);
+  static const Color surface = Color(0xFFFFF8F5);
+  static const Color onSurface = Color(0xFF241912);
+  static const Color surfaceVariant = Color(0xFFF4DED2);
+  static const Color onSurfaceVariant = Color(0xFF574335);
+  static const Color outline = Color(0xFF8A7262);
+  static const Color outlineVariant = Color(0xFFDEC1AE);
+  static const Color surfaceContainerLowest = Color(0xFFFFFFFF);
+  static const Color surfaceContainerLow = Color(0xFFFFF1E9);
+  static const Color surfaceContainer = Color(0xFFFFEADD);
+  static const Color surfaceContainerHigh = Color(0xFFFAE4D7);
+  static const Color surfaceContainerHighest = Color(0xFFF4DED2);
+  static const Color error = Color(0xFFBA1A1A);
+  static const Color onError = Color(0xFFFFFFFF);
+  static const Color errorContainer = Color(0xFFFFDAD6);
+  static const Color onErrorContainer = Color(0xFF93000A);
+  static const Color infoContainerBackground = Color(0xFFe1f5fe);
+  static const Color infoContainerBorder = Color(0xFFb3e5fc);
+  static const Color infoContainerText = Color(0xFF01579b);
+  static const Color infoContainerIcon = Color(0xFF0288d1);
 }
