798740188b9075716283efc2c52bc13001bcf790
CodeRonin404 <tukusalifyasichali@gmail.com>
2026-06-28 00:41:08 +0200

    Implemented the connection provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:vlc_remote/Providers/ConnectionProvider.dart';
 import 'package:vlc_remote/Screens/MainScreen.dart';
 
 void main() {
@@ -12,21 +14,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => Connectionprovider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
+        ),
 
-      home:
-          AnnotatedRegion<SystemUiOverlayStyle>(
-          value:SystemUiOverlayStyle(
-          systemNavigationBarColor: Colors.white,
-          systemNavigationBarIconBrightness: Brightness.light,
-          ),
-           child: const Mainscreen(),
-          )
+        home:
+            AnnotatedRegion<SystemUiOverlayStyle>(
+            value:SystemUiOverlayStyle(
+            systemNavigationBarColor: Colors.white,
+            systemNavigationBarIconBrightness: Brightness.light,
+            ),
+             child: const Mainscreen(),
+            )
+      ),
     );
   }
 }
